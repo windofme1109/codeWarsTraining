39eda49a83e193dbcf9f88b69c185894d844310d
windofme1109 <wuzengyan1314@163.com>
2021-01-20 23:25:34 +0800

    完成 6kyu 的题目 - 找到出现奇数次数的元素

--- Katas/21-01-20/2-FindTheOddInt.js
+++ Katas/21-01-20/2-FindTheOddInt.js
@@ -6,7 +6,34 @@
  *
  */
 
+/**
+ * 从一个数组中找出出现奇数次数的元素
+ * @param A
+ * @returns {number}
+ */
 function findOdd(A) {
-    //happy coding!
-    return 0;
+    // 1. 数组去重
+    const arrWithoutRepeatElement = [...(new Set(A))];
+
+    // 2. 遍历去重后的数组
+    let counts = arrWithoutRepeatElement.map((item) => {
+        let count = 0;
+        // 3. 获得元素的重复次数
+        A.forEach(e => {
+            if (e === item) {
+                count += 1;
+            }
+        });
+
+        return count;
+    });
+
+    // 找到出现奇数次数的元素的索引
+    let targetIndex = counts.findIndex(item => item % 2 === 1);
+
+    return arrWithoutRepeatElement[targetIndex];
 }
+
+console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
+console.log(findOdd([1,1,2,-2,5,2,4,4,-1,-2,5]))
+console.log(findOdd([10]))
