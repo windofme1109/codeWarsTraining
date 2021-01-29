f288554db5d18e26d333c5970544f9f157aacaa7
qinmr <qinmr@yonyou.com>
2021-01-29 15:21:52 +0800

    21年1月29日 刷题 6kyu 使用字母在字母表中出现的位置替换字母

---
+++ Katas/21-01-29/3-ReplaceWithAlphabetPosition.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * Welcome.
+ *
+ * In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+ *
+ * If anything in the text isn't a letter, ignore it and don't return it.
+ *
+ * "a" = 1, "b" = 2, etc.
+ *
+ * Example
+ * alphabetPosition("The sunset sets at twelve o' clock.")
+ * Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+ *
+ */
+
+function alphabetPosition(text) {
+    // 将字符串中的字母全部转为小写
+    // 使用正则表达式，去除字符串中的非字母字符
+    // 最后将其转换为数组
+    const textWithUpperCase = text.toLowerCase().replace(/[^a-z]/g, '').split('');
+
+    const positionArr = textWithUpperCase.map(char => {
+        // 获取小写字母的 ascii 码，小写字母的 ascii 范围是 97 - 123
+        // ascii 码减去 96 就是小写字母在字母表出现的位置
+        return char.charCodeAt(0) - 96;
+    })
+
+    return positionArr.join(' ');
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."))
