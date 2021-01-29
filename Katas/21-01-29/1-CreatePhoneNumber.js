d5b239219fcf408db9227c1e9d65db8ba1fb81f2
qinmr <qinmr@yonyou.com>
2021-01-29 15:20:11 +0800

    21年1月29日 刷题 6kyu 对电话号码进行格式化

---
+++ Katas/21-01-29/1-CreatePhoneNumber.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+ * Example:
+ * createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+ * The returned format must be correct in order to complete this challenge.
+ * Don't forget the space after the closing parentheses!
+ *
+ */
+
+// function createPhoneNumber(numbers){
+//     if (numbers.length !== 10) {
+//         return;
+//     }
+//
+//     return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`;
+// }
+
+
+function createPhoneNumber(numbers){
+    if (numbers.length !== 10) {
+        return;
+    }
+    const pattern = /(\d{3})(\d{3})(\d{4})/g;
+    return numbers.join('').replace(pattern, `($1) $2-$3`);
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+console.log(createPhoneNumber([1, 2, 3, 8, 7, 8, 4, 8, 9, 0]));
