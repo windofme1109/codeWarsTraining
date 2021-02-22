daeb685e52ef0f52aecc3d90c8f86d07be76c9bb
qinmr <qinmr@yonyou.com>
2021-02-22 09:58:05 +0800

    21年2月22日 刷题 6kyu 优化函数实现

--- Katas/21-02-21/3-SortTheOdd.js
+++ Katas/21-02-21/3-SortTheOdd.js
@@ -11,13 +11,18 @@
  */
 
 function sortArray(array) {
-    const oddArr = [];
+    // const oddArr = [];
 
-    for (let i = 0; i < array.length; i++) {
-        if (Math.abs(array[i] % 2) === 1) {
-            oddArr.push(array[i]);
-        }
-    }
+    // for (let i = 0; i < array.length; i++) {
+    //     if (Math.abs(array[i] % 2) === 1) {
+    //         oddArr.push(array[i]);
+    //     }
+    // }
+
+    // 测试用例中，不仅有正奇数，还有负奇数，所以我们可以使用 filter() 方法进行优化
+    // 过滤出数组的奇数，被 2 整除，余数为 0，所以是偶数，而 0 的布尔值为 false
+    // 所以这样能直接过滤出奇数
+    const oddArr = array.filter(item => item % 2);
 
     oddArr.sort((a, b) => a - b);
 
