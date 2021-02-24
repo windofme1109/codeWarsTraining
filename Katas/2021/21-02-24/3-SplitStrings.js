9cc5fbc3807fac77eb9a2a6c42334a626f5a598e
qinmr <qinmr@yonyou.com>
2021-02-24 16:41:39 +0800

    21年2月24日 刷题 6kyu 字符串两两分组

---
+++ Katas/2021/21-02-24/3-SplitStrings.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+ *
+ * Examples:
+ *
+ * solution('abc') // should return ['ab', 'c_']
+ * solution('abcdef') // should return ['ab', 'cd', 'ef']
+ *
+ */
+
+function solution(str){
+    const newStr = str.length % 2 === 0 ? str : str + '_';
+    let ret = newStr.match(/(.{2})/g);
+    return  ret ? ret : [];
+}
+
+console.log('abcd'.match(/(.{2})/g));
